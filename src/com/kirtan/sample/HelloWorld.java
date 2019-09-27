05302966e507123e8e5e78274f66fa61d993081a
kap199297 <kap199297@gmail.com>
2019-09-27 11:29:39 +0530

    testing

--- src/com/kirtan/sample/HelloWorld.java
+++ src/com/kirtan/sample/HelloWorld.java
@@ -2,6 +2,6 @@ package com.kirtan.sample;
 
 public class HelloWorld{
   public static void main(String args[]){
-    //System.out.println("Hello World !!! -From Kirtan Command New updated");
+    System.out.println("Hello World !!! -From Kirtan Command New updated");
   }
 }
