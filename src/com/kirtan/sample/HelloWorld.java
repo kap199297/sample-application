5b160b572e7ec792b914a0021e1bb3c5f9e3750f
Kirtan <kap199297@gmail.com>
2019-12-10 23:15:17 +0530

    Not Required Changes

--- src/com/kirtan/sample/HelloWorld.java
+++ src/com/kirtan/sample/HelloWorld.java
@@ -2,7 +2,7 @@ package com.kirtan.sample;
 
 public class HelloWorld{
   public static void main(String args[]){
-	//Branch A Comment
+	//Branch A Comment Not required.
     System.out.println("Hello World !!! -From Kirtan Command New updated");
   }
 }
